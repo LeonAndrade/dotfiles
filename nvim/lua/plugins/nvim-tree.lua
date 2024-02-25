7c8eaf13d041ec4ad4808f3ed34efcfe32a3bf9e
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2024-02-25 16:05:42 -0300

    Improving nvim-tree config setup

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -11,17 +11,43 @@ return {
     end
     
     nvimtree.setup {
+      on_attach = "defaullt",
+      hijack_cursor = false,
+      auto_reload_on_write = true,
+      disable_netrw = false,
+      hijack_netrw = true,
+      hijack_unnamed_buffer_when_opening = false,
       sort = {
         sorter = "case_sensitive",
+        folders_first = true,
+        files_first = false,
       },
       view = {
+        side = "left",
         width = 42,
+        number = false,
+        relativenumber = false,
       },
       renderer = {
-        group_empty = true,
+        group_empty = false,
       },
       filters = {
-        dotfiles = true,
+        git_ignored = true,
+        dotfiles = false,
+        git_clean = false,
+        no_buffer = false,
+        no_bookmark = false,
+      },
+      actions = {
+        expand_all = {
+          max_folder_discovery = 50,
+        },
+        open_file = {
+          resize_window = true,
+          window_picker = {
+            chars = "fkvneiru",
+          },
+        }
       }
     }
 
