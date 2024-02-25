651aa0c4f2ff57274e0e9b3f46cabbba1be88c36
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2024-02-25 16:06:55 -0300

    Neodev plugin from the amazing folke

---
+++ nvim/lua/plugins/neodev.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/neodev.nvim",
+  opts = {},
+  config = function ()
+    local status, neodev = pcall(require, "neodev")
+    if not status then
+      print("Error loading neodev")
+    end
+    neodev.setup {}
+  end
+
+}
