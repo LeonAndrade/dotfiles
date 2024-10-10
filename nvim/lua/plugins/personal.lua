dd0ff350917281f70d70d3bfacffc7142da7d4e6
LeonAndrade <35314330+LeonAndrade@users.noreply.github.com>
2024-10-10 16:53:29 -0300

    Adding my custom dataform.nvim plugin

---
+++ nvim/lua/plugins/personal.lua
@@ -0,0 +1,10 @@
+return {
+	"LeonAndrade/dataform.nvim",
+	config = function()
+		local dataform_status, dataform = pcall(require, "dataform")
+		if not dataform_status then
+			print("error loading dataform.nvim")
+		end
+		dataform.setup()
+	end,
+}
